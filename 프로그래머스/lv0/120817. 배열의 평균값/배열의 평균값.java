8540cc5a63810474d27dd8167f15ce5f1618c9cf
CatJelly <70837543+CatJelly@users.noreply.github.com>
2023-02-22 11:27:09 +0900

    [level 0] Title: 배열의 평균값, Time: 2.91 ms, Memory: 71.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120817. 배열의 평균값/배열의 평균값.java
+++ 프로그래머스/lv0/120817. 배열의 평균값/배열의 평균값.java
@@ -2,6 +2,6 @@ import java.util.Arrays;
 
 class Solution {
     public double solution(int[] numbers) {
-        return (double)Arrays.stream(numbers).sum() / numbers.length;
+        return Arrays.stream(numbers).average().getAsDouble();
     }
 }
