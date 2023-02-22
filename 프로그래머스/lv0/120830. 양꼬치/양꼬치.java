a2f25d048d684ecf987511c8a07c91d7e7c39281
CatJelly <70837543+CatJelly@users.noreply.github.com>
2023-02-22 13:51:57 +0900

    [level 0] Title: 양꼬치, Time: 0.01 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120830. 양꼬치/양꼬치.java
+++ 프로그래머스/lv0/120830. 양꼬치/양꼬치.java
@@ -1,6 +1,7 @@
 class Solution {
     public int solution(int n, int k) {
-        int answer = 0;
-        return answer;
+        int YKC = 12_000;
+        int DRK = 2_000;
+        return YKC * n + DRK * k - (n/10) * DRK;
     }
 }
