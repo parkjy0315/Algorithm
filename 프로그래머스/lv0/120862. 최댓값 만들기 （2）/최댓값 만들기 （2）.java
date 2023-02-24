cc83537815ee99004ca3acf0166799a22d5c78e9
박진용 <70837543+CatJelly@users.noreply.github.com>
2023-02-25 03:11:56 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.53 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] numbers) {
-        int [] arr = Arrays.stream(numbers).sorted().toArray();
-        return arr[0] * arr[1] > arr[arr.length - 1] * arr[arr.length - 2] ? arr[0] * arr[1] : arr[arr.length - 1] * arr[arr.length - 2];
+        Arrays.sort(numbers);
+        return Math.max(numbers[0] * numbers[1], numbers[numbers.length - 1] * numbers[numbers.length - 2]);
     }
 }
