ac213eb3ad07493af6e44b7ff06a2c0a633f4787
박진용 <70837543+CatJelly@users.noreply.github.com>
2023-02-24 10:19:45 +0900

    [level 0] Title: 배열의 유사도, Time: 2.98 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.java
+++ 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.java
@@ -1,24 +1,8 @@
 import java.util.*;
+import java.util.stream.Collectors;
 
 class Solution {
     public int solution(String[] s1, String[] s2) {
-        int answer = 0;
-        Set<String> set1 = new HashSet<String>(Arrays.asList(s1));
-        Set<String> set2 = new HashSet<String>(Arrays.asList(s2));
-        
-        if (s1.length > s2.length) {
-            for(int i=0; i<s2.length; i++) {
-                if (set1.contains(s2[i]))
-                    answer++;
-            }
-        }
-        else {
-            for(int i=0; i<s1.length; i++) {
-                if (set2.contains(s1[i]))
-                    answer++;
-            }
-        }
-        
-        return answer;
+        return (int) Arrays.stream(s1).map(s -> Arrays.stream(s2).collect(Collectors.toList()).contains(s)).filter(b -> b).count();
     }
 }
