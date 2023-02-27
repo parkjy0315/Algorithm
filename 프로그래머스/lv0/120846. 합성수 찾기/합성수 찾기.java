b4dea124d9cd1ffe00eeabc16b3de0c49a6fcf87
박진용 <70837543+CatJelly@users.noreply.github.com>
2023-02-27 14:44:32 +0900

    [level 0] Title: 합성수 찾기, Time: 1.56 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.java
+++ 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.java
@@ -2,8 +2,6 @@ import java.util.stream.*;
 
 class Solution {
     public int solution(int n) {
-        return (int)IntStream.rangeClosed(1, n)
-            .filter(i -> (int)IntStream.rangeClosed(1, i).filter(j -> i % j == 0).count() > 2)
-            .count();
+        return (int)IntStream.rangeClosed(1, n).filter(i -> (int)IntStream.rangeClosed(1, i).filter(j -> i % j == 0).count() > 2).count();
     }
 }
