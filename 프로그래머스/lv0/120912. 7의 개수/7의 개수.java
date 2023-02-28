003c25dcc7d041676c8ccf413ccdd182c26be56c
박진용 <70837543+CatJelly@users.noreply.github.com>
2023-02-28 16:11:20 +0900

    [level 0] Title: 7의 개수, Time: 4.11 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120912. 7의 개수/7의 개수.java
+++ 프로그래머스/lv0/120912. 7의 개수/7의 개수.java
@@ -1,16 +1,15 @@
 import java.util.*;
+import java.util.stream.*;
 
 class Solution {
     public int solution(int[] array) {
-       int[] temp = new int[array.length];
-        for(int i=0; i<array.length; i++) {
-            String tmp = String.valueOf(array[i]);
-            temp[i] = (int)tmp.chars().filter(c -> c == '7').count();
-        }
-        
-        int sum = 0;
-        for(int v : temp) 
-            sum += v;
-        return sum;
+       return (int) Arrays.stream(
+                        Arrays.stream(array)
+                                .mapToObj(String::valueOf)
+                                .collect(Collectors.joining())
+                                .split("")
+                )
+                .filter(s -> s.equals("7"))
+                .count();
     }
 }
