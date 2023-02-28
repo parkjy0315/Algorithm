7978081400dd2c4ce50681e5f1ccb33fbe4c4d49
박진용 <70837543+CatJelly@users.noreply.github.com>
2023-02-28 11:36:05 +0900

    [level 0] Title: 진료 순서 정하기, Time: 4.31 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120835. 진료 순서 정하기/진료 순서 정하기.java
+++ 프로그래머스/lv0/120835. 진료 순서 정하기/진료 순서 정하기.java
@@ -1,25 +1,13 @@
 import java.util.*;
+import java.util.stream.*;
 
 class Solution {
-    public int findIndex(int [] arr, int value) {
-        for(int i=0; i<arr.length; i++) {
-            if(arr[i] == value)
-                return i;
-        }
-        return -1;
-    }
-    
-    public int[] solution(int[] emergency) {
-        int length = emergency.length;
-        int[] answer = new int[length];
-        Integer [] temp = Arrays.stream(emergency).boxed().toArray(Integer[]::new);
-        Arrays.sort(temp, Comparator.reverseOrder());
-        
-        for(int i=0; i<length; i++) {
-            int index = findIndex(emergency, temp[i]);
-            answer[index] = i+1;
-        }
-        
-        return answer;
+    public int [] solution(int[] e) {
+        return Arrays.stream(e).map(
+                i -> Arrays.stream(e).boxed()
+                .sorted(Comparator.reverseOrder())
+                .collect(Collectors.toList())
+                .indexOf(i) + 1)
+            .toArray();
     }
 }
