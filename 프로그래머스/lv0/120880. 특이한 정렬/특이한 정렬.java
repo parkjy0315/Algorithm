cb5dd5fd5e7e8d11ae1972ed320eefe7b15dd17f
박진용 <70837543+CatJelly@users.noreply.github.com>
2023-03-04 03:43:42 +0900

    [level 0] Title: 특이한 정렬, Time: 5.59 ms, Memory: 70 MB -BaekjoonHub

--- 프로그래머스/lv0/120880. 특이한 정렬/특이한 정렬.java
+++ 프로그래머스/lv0/120880. 특이한 정렬/특이한 정렬.java
@@ -1,10 +1,15 @@
 import java.util.Arrays;
 
+
 class Solution {
     public int[] solution(int[] numList, int n) {
         return Arrays.stream(numList)
+                .map(i -> i - n)
                 .boxed()
-                .sorted((a, b) -> Math.abs(a - n) == Math.abs(b - n) ? b.compareTo(a) : Integer.compare(Math.abs(a - n), Math.abs(b - n)))
+                .sorted((e1, e2) -> {
+                    return Math.abs(e1) == Math.abs(e2) ? e2.compareTo(e1) : Integer.compare(Math.abs(e1), Math.abs(e2));
+                })
+                .map(i -> i + n)
                 .mapToInt(Integer::intValue)
                 .toArray();
     }
