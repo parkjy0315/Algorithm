e45de830bf0b6bbe8cae84edb8b14ac13a5fd75b
박진용 <70837543+CatJelly@users.noreply.github.com>
2023-03-03 17:12:14 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.02 ms, Memory: 77.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] solution(String[] keyinput, int[] board) {
+        int [] answer = {0, 0};
+        for(String key : keyinput) {
+            switch(key) {
+                case "up":
+                    if(!(answer[1] + 1 > board[1] / 2))
+                        answer[1]++;
+                    break;
+                case "down":
+                    if(!(answer[1] - 1 < -board[1] / 2))
+                        answer[1]--;
+                    break;
+                case "left":
+                    if(!(answer[0] - 1 < -board[0] / 2))
+                        answer[0]--;
+                    break;
+                case "right":
+                    if(!(answer[0] + 1 > board[0] / 2))
+                        answer[0]++;
+                    break;
+            }
+        }
+        return answer;
+    }
+}
