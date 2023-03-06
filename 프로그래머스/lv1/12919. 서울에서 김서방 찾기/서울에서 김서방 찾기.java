25e2e03be291846c7c4452cc7a528129bcec5e4f
박진용 <70837543+CatJelly@users.noreply.github.com>
2023-03-06 17:53:01 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 1.06 ms, Memory: 81.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
+++ 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -1,9 +1,7 @@
+import java.util.*;
+
 class Solution {
     public String solution(String[] seoul) {
-        for(int i=0; i<seoul.length; i++) {
-            if(seoul[i].equals("Kim"))
-                return String.format("김서방은 %d에 있다", i);
-        }
-               return String.format("김서방은 %d에 있다", -1);
+        return String.format("김서방은 %d에 있다", Arrays.asList(seoul).indexOf("Kim"));
     }
 }
