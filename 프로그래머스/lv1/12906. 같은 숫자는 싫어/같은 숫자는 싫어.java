83411c24fe43e61169e9f269d88444f76d873a02
박진용 <70837543+CatJelly@users.noreply.github.com>
2023-03-07 23:47:12 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 44.46 ms, Memory: 117 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int []arr) {
+        List<Integer> list = new ArrayList<>();
+        list.add(arr[0]);
+        for(int i=1; i<arr.length; i++) {
+            if(list.get(list.size() - 1) != arr[i])
+                list.add(arr[i]);
+        }
+
+        return list.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
