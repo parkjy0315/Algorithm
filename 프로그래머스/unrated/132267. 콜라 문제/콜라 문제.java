418d6f7a2ed87a573028bd94d337580e8d1fe7dd
박진용 <70837543+CatJelly@users.noreply.github.com>
2023-03-12 18:49:28 +0900

    [unrated] Title: 콜라 문제, Time: 0.02 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.java
+++ 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.java
@@ -1,9 +1,10 @@
 class Solution {
     public int solution(int a, int b, int n) {
-        int answer = 0;
         // 콜라가 a개 미만이 아닌 경우 계속 반복
         // 콜라 빈병을 a개 주면
         // 콜라 b병을 새로줌
+        
+        int answer = 0;
         while(!(n < a)) {
             n -= a;
             n += b;
