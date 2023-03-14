914c5223463e22adfd5bf166a48acf8d6e7e2a81
박진용 <70837543+CatJelly@users.noreply.github.com>
2023-03-14 13:04:42 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 314.67 ms, Memory: 95.8 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,16 +1,16 @@
 import java.util.*;
-import java.util.stream.Collectors;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        List<String> pList = Arrays.stream(participant).sorted().collect(Collectors.toList());
-        List<String> cList = Arrays.stream(completion).sorted().collect(Collectors.toList());
-
-        for (int i = 0; i < cList.size(); i++) {
-            if(!pList.get(i).equals(cList.get(i))) {
-                return pList.get(i);
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+        
+        int i;
+        for (i = 0; i < completion.length; i++) {
+            if(!participant[i].equals(completion[i])) {
+                return participant[i];
             }
         }
-        return pList.get(pList.size() - 1);
+        return participant[i];
     }
 }
