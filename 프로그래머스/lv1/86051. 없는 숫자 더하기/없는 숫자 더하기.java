31fbaa551b2b85c45ab5b84c046d3d2ffde29814
박진용 <70837543+CatJelly@users.noreply.github.com>
2023-03-14 17:21:48 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 2.04 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -3,7 +3,6 @@ import java.util.stream.*;
 
 class Solution {
     public int solution(int[] numbers) {
-        List<Integer> list = IntStream.rangeClosed(0, 9).boxed().collect(Collectors.toList());
-        return list.stream().filter(i -> Arrays.stream(numbers).filter(j -> i == j).count() == 0).mapToInt(Integer::intValue).sum();
+        return IntStream.rangeClosed(0, 9).filter(i -> Arrays.stream(numbers).filter(j -> i == j).count() == 0).sum();
     }
 }
