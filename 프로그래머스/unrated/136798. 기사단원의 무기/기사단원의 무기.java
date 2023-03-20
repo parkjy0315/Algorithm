63cdc1049ddf6c5e0d0727442a070675acfba3dd
박진용 <70837543+CatJelly@users.noreply.github.com>
2023-03-20 17:35:08 +0900

    [unrated] Title: 기사단원의 무기, Time: 1.80 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.java
+++ 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.java
@@ -1,20 +1,6 @@
 class Solution {
-    public int getDivisorCount(int num) {
-        int count = 1;
-        for(int i=1; i<=num / 2; i++) {
-            if (num % i == 0)
-                count++;
-        }
-
-        return count;
-    }
-
     public int solution(int number, int limit, int power) {
         int answer = 0;
-        for(int i=1; i<=number; i++) {
-            int count = getDivisorCount(i);
-            answer += count > limit ? power : count;
-        }
         return answer;
     }
 }
