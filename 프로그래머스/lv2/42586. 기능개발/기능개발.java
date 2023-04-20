7c8504dd64bc122a38e5322c31357ef73f758cd8
박진용 <70837543+CatJelly@users.noreply.github.com>
2023-04-20 14:34:04 +0900

    [level 2] Title: 기능개발, Time: 0.03 ms, Memory: 69.9 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.java
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.java
@@ -1,15 +1,38 @@
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.*;
+
 class Solution {
-    public int[] solution(int[] progresses, int[] speeds) {
-        int[] dayOfend = new int[100];
-        int day = -1;
-        for(int i=0; i<progresses.length; i++) {
-            while(progresses[i] + (day*speeds[i]) < 100) {
-                day++;
+    public List<Integer> solution(int[] progresses, int[] speeds) {
+        // 배포 배열 리스트
+        List<Integer> delivery = new ArrayList<>();
+        int length = progresses.length;
+        // 배포는 하루에 한번, 하루 끝에 진행
+        // 처음 인덱스부터 진행해 각 speeds 만큼 각각 추가
+        int index = 0;
+        int count = 0;
+        while (index != length) {
+            // 현재 인덱스가 100일 때
+            if (progresses[index] >= 100) {
+                count++;
+                // 현재 인덱스 바로 뒤부터 100인 경우
+                for (int i = index + 1; i < length; i++) {
+                    if (progresses[i] >= 100) {
+                        count++;
+                        index = i;
+                    } else {
+                        break;
+                    }
+                }
+                // 모두 같이 배포
+                delivery.add(count);
+                count = 0;
+                index++;
             }
-            dayOfend[day]++;
+
+            for (int i = index; i < length; i++) {
+                progresses[i] += speeds[i];
+            }
         }
-        return Arrays.stream(dayOfend).filter(i -> i!=0).toArray();
+
+        return delivery;
     }
 }
