31f0058ebbf55ac2d9e6951755c14f4a02706a21
박진용 <70837543+CatJelly@users.noreply.github.com>
2023-04-21 15:09:01 +0900

    [level 2] Title: 더 맵게, Time: 1927.16 ms, Memory: 137 MB -BaekjoonHub

--- 프로그래머스/lv2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/lv2/42626. 더 맵게/더 맵게.java
@@ -1,31 +1,22 @@
 import java.util.PriorityQueue;
 
 class Solution {
-    public boolean check(PriorityQueue<Double> minHeap, int k) {
-        for (double value : minHeap) {
-            if (value < k)
-                return false;
-        }
-        return true;
-    }
     public int solution(int[] scoville, int k) {
         PriorityQueue<Double> minHeap = new PriorityQueue<>() {{
             for (int i = 0; i < scoville.length; i++) add((double)scoville[i]);
         }};
 
         int answer = 0;
-        while (!check(minHeap, k)) {
-            if (minHeap.size() >= 2) {
-                double lowest = minHeap.peek(); minHeap.remove();
-                double lowest2 = minHeap.peek(); minHeap.remove();
-                minHeap.add(lowest + lowest2 * 2);
-                answer++;
-            } else {
-                answer = -1;
-                break;
-            }
+        while (minHeap.size() >= 2 && minHeap.peek() < k) {
+            double lowest = minHeap.peek(); minHeap.remove();
+            double lowest2 = minHeap.peek(); minHeap.remove();
+            minHeap.add(lowest + lowest2 * 2);
+            answer++;
         }
 
+        if (minHeap.size() <= 1 && minHeap.peek() < k)
+            answer = -1;
+        
         return answer;
     }
 }
