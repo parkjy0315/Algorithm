55542f47cc36f1acdfe4aa3b51d3d00a90cf38b1
박진용 <70837543+CatJelly@users.noreply.github.com>
2023-04-25 09:24:53 +0900

    [Bronze III] Title: 최소, 최대, Time: 584 ms, Memory: 109988 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int [] input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        System.out.println(Arrays.stream(input).min().getAsInt() + " " + Arrays.stream(input).max().getAsInt());
+    }
+}
