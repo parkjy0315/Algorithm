9edbdcb03048cad2c40055c690318600f6d3253c
박진용 <70837543+CatJelly@users.noreply.github.com>
2023-05-05 04:28:25 +0900

    [Bronze V] Title: 행렬 덧셈, Time: 488 ms, Memory: 22292 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -0,0 +1,30 @@
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[][] matrix = new int[N][M];
+        for (int test = 0; test < 2; test++) {
+            for (int i = 0; i < N; i++) {
+                st = new StringTokenizer(br.readLine(), " ");
+                for (int j = 0; j < M; j++) {
+                    matrix[i][j] += Integer.parseInt(st.nextToken());
+                }
+            }
+        }
+
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                System.out.printf(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
