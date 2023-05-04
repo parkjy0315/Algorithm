4ece668c8facbe613cee6556c46dd5442c55bba1
박진용 <70837543+CatJelly@users.noreply.github.com>
2023-05-05 04:02:18 +0900

    [Bronze V] Title: 새싹, Time: 120 ms, Memory: 14028 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println("""
+                         ,r'"7
+                r`-_   ,'  ,/
+                 \\. ". L_r'
+                   `~\\/
+                      |
+                      |
+                
+                """);
+    }
+}
