5761f8a88405f0b7ea4e8c7aebf00326327083c3
박진용 <70837543+CatJelly@users.noreply.github.com>
2023-05-08 10:01:29 +0900

    [D1] Title: 신문 헤드라인, Time: 97 ms, Memory: 18,340 KB -BaekjoonHub

---
+++ SWEA/D1/2047. 신문 헤드라인/신문 헤드라인.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+class Solution
+{
+    public static void main(String args[]) throws Exception
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+
+        System.out.println(input.toUpperCase());
+    }
+}
