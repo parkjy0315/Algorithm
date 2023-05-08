c6d7b1cd812598a69784f5c6f0f94000d7386641
박진용 <70837543+CatJelly@users.noreply.github.com>
2023-05-08 17:39:40 +0900

    [D3] Title: 모음이 보이지 않는 사람, Time: 106 ms, Memory: 18,688 KB -BaekjoonHub

---
+++ SWEA/D3/4406. 모음이 보이지 않는 사람/모음이 보이지 않는 사람.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+class Solution {
+    public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        for (int test = 1; test <= T; test++) {
+            System.out.printf("#%d ", test);
+
+            String input = br.readLine();
+            System.out.println(input.replaceAll("a|e|i|o|u", ""));
+        }
+    }
+}
