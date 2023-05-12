fc37780ffc9c6bc12b034a68a755df3df74686f6
박진용 <70837543+CatJelly@users.noreply.github.com>
2023-05-12 17:10:31 +0900

    [D1] Title: 더블더블, Time: 107 ms, Memory: 19,404 KB -BaekjoonHub

---
+++ SWEA/D1/2019. 더블더블/더블더블.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+class Solution {
+    public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        for(int i=0; i<= N; i++) {
+            System.out.print((int)Math.pow(2, i) + " ");
+        }
+    }
+}
