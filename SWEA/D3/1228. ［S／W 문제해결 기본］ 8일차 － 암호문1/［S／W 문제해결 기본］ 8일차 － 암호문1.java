141e29453eac209225bb808750bf5ac82dd4e814
박진용 <70837543+CatJelly@users.noreply.github.com>
2023-05-15 16:23:33 +0900

    [D3] Title: [S/W 문제해결 기본] 8일차 - 암호문1, Time: 197 ms, Memory: 28,360 KB -BaekjoonHub

---
+++ SWEA/D3/1228. ［S／W 문제해결 기본］ 8일차 － 암호문1/［S／W 문제해결 기본］ 8일차 － 암호문1.java
@@ -0,0 +1,57 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.util.stream.Collectors;
+
+class Solution {
+    public static void insert(List<String> cryptoList, int index, int num, String[] numArr) {
+        for (int i = index; i < index + num; i++) {
+            String temp = numArr[i - index];
+            cryptoList.add(i, temp);
+        }
+    }
+
+    public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        for (int test = 1; test <= 10; test++) {
+            System.out.printf("#%d ", test);
+
+            // 입력 처리
+            int N = Integer.parseInt(br.readLine());
+            List<String> cryptoList = new ArrayList<>(Arrays.stream(br.readLine()
+                    .split(" ")).collect(Collectors.toList()));
+            int M = Integer.parseInt(br.readLine());
+            List<String> commandList = new ArrayList<>(Arrays.stream(br.readLine()
+                    .split(" ")).collect(Collectors.toList()));
+
+            // 명령어 처리
+            while (!commandList.isEmpty()) {
+                String command = commandList.get(0);
+                commandList.remove(0);
+
+                int x, y;
+                String[] s = null;
+                switch (command) {
+                    case "I": // 삽입
+                        x = Integer.parseInt(commandList.get(0));
+                        commandList.remove(0);
+                        y = Integer.parseInt(commandList.get(0));
+                        commandList.remove(0);
+                        s = new String[y];
+                        for (int i = 0; i < y; i++) {
+                            s[i] = commandList.get(0);
+                            commandList.remove(0);
+                        }
+                        insert(cryptoList, x, y, s);
+                        break;
+                }
+            }
+
+            for (int i=0; i<10; i++)
+                System.out.print(cryptoList.get(i) + " ");
+            System.out.println();
+        }
+
+    }
+}
