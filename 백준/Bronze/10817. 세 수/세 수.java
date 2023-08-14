8c40538bf4918418296a9894c8ee1d5b5eaa73af
박진용 <70837543+CatJelly@users.noreply.github.com>
2023-08-14 22:22:21 +0900

    [Bronze III] Title: 세 수, Time: 148 ms, Memory: 15936 KB -BaekjoonHub

---
+++ 백준/Bronze/10817. 세 수/세 수.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws Exception {
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int[] arr = new int[3];
+
+        for (int i = 0; i < 3; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        bw.write(arr[1] + "\n");
+
+        bw.close();
+        br.close();
+    }
+}
