393e9c00ed1e450a3b744a2b3875bc7334f6790b
박진용 <70837543+CatJelly@users.noreply.github.com>
2023-08-28 19:04:10 +0900

    [Gold V] Title: LCS, Time: 196 ms, Memory: 19352 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws Exception {
+        String str1 = br.readLine();
+        String str2 = br.readLine();
+
+        int len1 = str1.length();
+        int len2 = str2.length();
+
+        int[][] LCS = new int[len1 + 1][len2 + 1];
+
+        int max = 0;
+
+        for (int i = 0; i < len1 + 1; i++) {
+            for (int j = 0; j < len2 + 1; j++) {
+                if (i == 0 || j == 0) {
+                    LCS[i][j] = 0;
+                } else if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
+                    LCS[i][j] = LCS[i - 1][j - 1] + 1;
+                } else {
+                    LCS[i][j] = Math.max(LCS[i - 1][j], LCS[i][j - 1]);
+                }
+
+                if (LCS[i][j] > max) {
+                    max = LCS[i][j];
+                }
+            }
+        }
+
+        bw.write(max + "\n");
+
+        bw.close();
+        br.close();
+    }
+}
+
