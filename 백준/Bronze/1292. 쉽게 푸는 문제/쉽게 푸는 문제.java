0de3ae3f4c6f1ff785cc9346698115a8a5cf266d
박진용 <70837543+CatJelly@users.noreply.github.com>
2023-11-09 12:16:00 +0900

    [Bronze I] Title: 쉽게 푸는 문제, Time: 148 ms, Memory: 14612 KB -BaekjoonHub

---
+++ 백준/Bronze/1292. 쉽게 푸는 문제/쉽게 푸는 문제.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws Exception {
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        int index = 1;
+        int i = 1;
+        int sum = 0;
+        while (true) {
+            for (int j=0; j<i; j++) {
+                if (A <= index && index <= B) {
+                    sum += i;
+                }
+                index++;
+            }
+            if (index > B) {
+                break;
+            }
+            i++;
+        }
+
+        bw.write(sum + "\n");
+
+        bw.close();
+        br.close();
+    }
+}
