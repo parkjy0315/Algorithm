74aa1899127c46ccedf5df6c793a8452ccfdd49e
박진용 <70837543+parkjy0315@users.noreply.github.com>
2025-05-18 00:05:08 +0900

    [Silver II] Title: 나무 자르기, Time: 920 ms, Memory: 141972 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.java
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -1,42 +1,48 @@
 import java.io.*;
 import java.util.*;
 
-class Main {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
+public class Main {
     public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
         String[] input = br.readLine().split(" ");
         int N = Integer.parseInt(input[0]);
         int M = Integer.parseInt(input[1]);
 
         int[] trees = Arrays.stream(br.readLine().split(" "))
-                .mapToInt(Integer::parseInt).sorted().toArray();
+                .mapToInt(Integer::parseInt)
+                .sorted()
+                .toArray();
 
-        int start = 0, end = trees[N - 1];
+        int start = 0;
+        int end = trees[N - 1];
+        int result = 0;
 
-        while (start < end) {
+        while (start <= end) {
             int mid = (start + end) / 2;
-            long total = 0;
 
-            for (int i=0; i<N; i++) {
-                int temp = trees[i] - mid;
-                if (temp > 0) {
-                    total += temp;
-                }
-            }
-
-            if (total < M) {
-                end = mid;
-            }
-            else {
+            if (isPossible(trees, mid, M)) {
+                result = mid;
                 start = mid + 1;
+            } else {
+                end = mid - 1;
             }
         }
 
-        bw.write(start - 1 + "\n");
+        bw.write(String.format("%d", result));
 
         bw.close();
         br.close();
+    }
+
+    private static boolean isPossible(int[] trees, int height, int minimum) {
+        long total = 0;
+
+        for (int tree : trees) {
+            total += tree > height ? tree - height : 0;
+        }
+
+        return total >= minimum;
     }
 }
