77d30b34044e266b3a2629315e4b3e3c51110b50
박진용 <70837543+parkjy0315@users.noreply.github.com>
2025-05-17 19:47:15 +0900

    [Gold IV] Title: 알파벳, Time: 984 ms, Memory: 18676 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -1,57 +1,53 @@
-import java.io.*;
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
-    private static int R, C;
-    private static String[][] board;
-    private static Set<String> visited = new HashSet<>();
-    private static int max = 0;
+    static int R, C;
+    static char[][] board;
+    static boolean[] visited = new boolean[26]; // 알파벳 A~Z 사용 여부
+    static int max = 0;
 
-    private static int[] dx = { -1, 1, 0, 0 };
-    private static int[] dy = { 0, 0, -1, 1 };
+    // 상하좌우 이동
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
 
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void dfs(int x, int y, int depth) {
+        // 현재 위치의 알파벳 방문 처리
+        visited[board[x][y] - 'A'] = true;
+        max = Math.max(max, depth);
 
-        // 입력 처리
-        String[] input = br.readLine().split(" ");
-        R = Integer.parseInt(input[0]);
-        C = Integer.parseInt(input[1]);
-
-        board = new String[R][C];
-
-        for (int i = 0; i < R; i++) {
-            board[i] = new String[C];
-            board[i] = br.readLine().split("");
-        }
-
-        // DFS, 백트랙킹 => 백트랙킹 return 조건에서 최대값 갱신 체크
-        dfs(0, 0, 1);
-        bw.write("" + max);
-
-        bw.close();
-        br.close();
-    }
-
-    private static void dfs(int x, int y, int count) {
-        String currentString = board[x][y];
-        visited.add(currentString);
-        max = Math.max(max, count);
-
-        for (int i = 0; i < dx.length; i++) {
+        // 4방향 탐색
+        for (int i = 0; i < 4; i++) {
             int nx = x + dx[i];
             int ny = y + dy[i];
 
-            if ((0 <= nx && nx < R) && (0 <= ny && ny < C)) {
-                String newString = board[nx][ny];
-                if (!visited.contains(newString)) {
-                    dfs(nx, ny, count + 1);
+            // 보드 범위 내 & 해당 알파벳 방문 안 했으면 이동
+            if (nx >= 0 && nx < R && ny >= 0 && ny < C) {
+                int nextChar = board[nx][ny] - 'A';
+                if (!visited[nextChar]) {
+                    dfs(nx, ny, depth + 1);
                 }
             }
         }
 
-        visited.remove(currentString);
+        // 백트래킹 (다음 경로를 위해 방문 해제)
+        visited[board[x][y] - 'A'] = false;
     }
 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        R = sc.nextInt();
+        C = sc.nextInt();
+        board = new char[R][C];
+
+        // 입력 처리
+        for (int i = 0; i < R; i++) {
+            String line = sc.next();
+            for (int j = 0; j < C; j++) {
+                board[i][j] = line.charAt(j);
+            }
+        }
+
+        dfs(0, 0, 1);  // (0,0)에서 시작, 깊이 1부터
+        System.out.println(max);
+    }
 }
