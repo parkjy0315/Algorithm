ecf4f3804aaba1dc72176bbd5d20c0aa478519a7
박진용 <70837543+parkjy0315@users.noreply.github.com>
2025-08-24 16:14:53 +0900

    [Silver II] Title: DFS와 BFS, Time: 316 ms, Memory: 25544 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -1,43 +1,19 @@
 import java.io.*;
 import java.util.*;
 
-class Main {
-    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+public class Main {
     private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-    public static void main(String args[]) throws Exception {
-        String[] input = br.readLine().split(" ");
-        int N = Integer.parseInt(input[0]);
-        int M = Integer.parseInt(input[1]);
-        int V = Integer.parseInt(input[2]);
-
-        int[][] graph = new int[N][N];
-
-        for (int i = 0; i < M; i++) {
-            String[] temp = br.readLine().split(" ");
-            int start = Integer.parseInt(temp[0]) - 1;
-            int end = Integer.parseInt(temp[1]) - 1;
-
-            graph[start][end] = graph[end][start] = 1;
-        }
-
-        dfs(graph, V);
-        bfs(graph, V);
-
-        bw.close();
-        br.close();
-    }
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));        
 
     public static void dfs(int[][] graph, int v) throws Exception {
-        Stack<Integer> stack = new Stack<>() {{
-            add(v);
-        }};
+        Stack<Integer> stack = new Stack<>() {{ push(v); }};
+
         int N = graph.length;
         boolean[] visited = new boolean[N];
 
-        while (!stack.isEmpty()) {
+        while(!stack.isEmpty()) {
             int current = stack.pop();
-
+            
             if (!visited[current - 1]) {
                 visited[current - 1] = true;
                 bw.write(current + " ");
@@ -49,17 +25,17 @@ class Main {
                 }
             }
         }
+
         bw.write("\n");
     }
 
     public static void bfs(int[][] graph, int v) throws Exception {
-        Queue<Integer> queue = new LinkedList<>() {{
-            add(v);
-        }};
+        Queue<Integer> queue = new LinkedList<>() {{ add(v); }};
+
         int N = graph.length;
         boolean[] visited = new boolean[N];
 
-        while (!queue.isEmpty()) {
+        while(!queue.isEmpty()) {
             int current = queue.poll();
 
             if (!visited[current - 1]) {
@@ -73,6 +49,29 @@ class Main {
                 }
             }
         }
+
         bw.write("\n");
+    }
+
+    public static void main(String[] args) throws Exception{
+        String[] input = br.readLine().split(" ");
+        int N = Integer.parseInt(input[0]);
+        int M = Integer.parseInt(input[1]);
+        int V = Integer.parseInt(input[2]);
+
+        int[][] graph = new int[N][N];
+        for (int i = 0; i < M; i++) {
+            String[] vertex = br.readLine().split(" ");
+            int from = Integer.parseInt(vertex[0]) - 1;
+            int to = Integer.parseInt(vertex[1]) - 1;
+
+            graph[from][to] = graph[to][from] = 1;
+        }
+        
+        dfs(graph, V);
+        bfs(graph, V);
+        
+        br.close();
+        bw.close();
     }
 }
